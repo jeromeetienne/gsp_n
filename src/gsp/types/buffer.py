818af41ca9726327e9a122358e54243b22796451
Jerome Etienne <jerome.etienne@gmail.com>
2025-10-28 11:27:33 +0100

    refactor: Clean up Buffer class formatting and improve assertions for data integrity

--- src/gsp/types/buffer.py
+++ src/gsp/types/buffer.py
@@ -5,9 +5,10 @@ from enum import Enum
 import numpy as np
 
 # local imports
-from . buffer_type import BufferType
+from .buffer_type import BufferType
 
-class Buffer():
+
+class Buffer:
     """typed array with single dimension
     - it is immutable in count and type, but mutable in content
     """
@@ -20,6 +21,18 @@ class Buffer():
 
     def __repr__(self) -> str:
         return f"Buffer(count={self._count}, type={self._type})"
+
+    def get_count(self) -> int:
+        """Return the number of elements in the buffer."""
+        return self._count
+
+    def get_type(self) -> BufferType:
+        """Return the type of each element in the buffer."""
+        return self._type
+
+    # =============================================================================
+    # .get_data/.set_data
+    # =============================================================================
 
     def get_data(self, offset: int, count: int) -> "Buffer":
         """Return a buffer of count elements starting from offset."""
@@ -36,31 +49,37 @@ class Buffer():
         item_size = BufferType.get_item_size(self._type)
 
         # sanity check
-        assert offset + count <= self._count, f"Invalid offset {offset} and count {count} for buffer of size {self._count}"
+        assert (
+            offset + count <= self._count
+        ), f"Invalid offset {offset} and count {count} for buffer of size {self._count}"
 
         start = offset * item_size
         end = start + count * item_size
-        self._bytearray = self._bytearray[:start] + _bytearray[0:count * item_size] + self._bytearray[end:]
+        self._bytearray = (
+            self._bytearray[:start]
+            + _bytearray[0 : count * item_size]
+            + self._bytearray[end:]
+        )
 
-    def get_count(self) -> int:
-        """Return the number of elements in the buffer."""
-        return self._count
-
-    def get_type(self) -> BufferType:
-        """Return the type of each element in the buffer."""
-        return self._type
+    # =============================================================================
+    # .to_numpy/.from_numpy
+    # =============================================================================
 
     # numpy conversion
     def to_numpy(self) -> np.ndarray:
-        if( self.get_type() == BufferType.rgba8 ):
+        if self.get_type() == BufferType.rgba8:
             # Special case for color buffer: interpret as uint8 and reshape
             numpy_dtype = np.dtype(np.uint8)
-            ndarray = np.frombuffer(self._bytearray, dtype=numpy_dtype).reshape((self._count, 4))
+            ndarray = np.frombuffer(self._bytearray, dtype=numpy_dtype).reshape(
+                (self._count, 4)
+            )
             return ndarray
-        
+
         numpy_dtype = BufferType.to_numpy_dtype(self._type)
         numpy_shape = BufferType.to_numpy_shape(self._type)
-        ndarray = np.frombuffer(self._bytearray, dtype=numpy_dtype).reshape((self._count, ) + numpy_shape)
+        ndarray = np.frombuffer(self._bytearray, dtype=numpy_dtype).reshape(
+            (self._count,) + numpy_shape
+        )
         return ndarray
 
     @staticmethod
@@ -72,6 +91,10 @@ class Buffer():
         buffer.set_data(bytearray(ndarray.tobytes()), 0, buffer.get_count())
         return buffer
 
+    # =============================================================================
+    # .to_bytearray/from_bytearray
+    # =============================================================================
+
     # bytes conversion
     def to_bytearray(self) -> bytearray:
         return bytearray(self._bytearray)
@@ -80,7 +103,9 @@ class Buffer():
     def from_bytearray(_bytearray: bytearray, buffer_type: BufferType) -> "Buffer":
         item_size = BufferType.get_item_size(buffer_type)
         # sanity check
-        assert len(_bytearray) % item_size == 0, f"data size {len(_bytearray)} is not aligned with buffer type item size {item_size}"
+        assert (
+            len(_bytearray) % item_size == 0
+        ), f"data size {len(_bytearray)} is not aligned with buffer type item size {item_size}"
 
         # create buffer
         buffer = Buffer(len(_bytearray) // item_size, buffer_type)
