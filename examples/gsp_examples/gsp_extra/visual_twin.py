a42ebdecf13e2dc200756557bf9981766290dc2c
Jerome Etienne <jerome.etienne@gmail.com>
2025-10-29 12:33:10 +0100

    fix: Reorder docstrings in VisualTwin class constructor for clarity

--- examples/gsp_examples/gsp_extra/visual_twin.py
+++ examples/gsp_examples/gsp_extra/visual_twin.py
@@ -6,5 +6,8 @@ from gsp.core.visual_base import VisualBase
 class VisualTwin:
     def __init__(self, visual: VisualBase):
         self.uuid = visual.uuid
-        self.model_matrix = Bufferx.mat4_identity()
+        """uuid of the visual being wrapped."""
         self.visual = visual
+        """the visual being wrapped."""
+        self.model_matrix = Bufferx.mat4_identity()
+        """model matrix of the visual being wrapped."""
