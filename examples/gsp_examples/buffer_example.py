326b857f0b84535f9927ee337d3959a65e9f734e
Jerome Etienne <jerome.etienne@gmail.com>
2025-10-31 16:56:05 +0100

    Remove unused import and standardize string quotes in buffer_example.py

--- examples/gsp_examples/buffer_example.py
+++ examples/gsp_examples/buffer_example.py
@@ -1,6 +1,5 @@
 # local imports
 from gsp.types import Buffer, BufferType
-from gsp_extra.bufferx import Bufferx
 
 
 def main():
@@ -8,7 +7,7 @@ def main():
     print("Buffer created:", buffer)
 
     # Set data
-    buffer.set_data(bytearray(b'\x00\x00\x00\x22' * 2), 0, 2)
+    buffer.set_data(bytearray(b"\x00\x00\x00\x22" * 2), 0, 2)
     print("Buffer data set.")
 
     # Get data
